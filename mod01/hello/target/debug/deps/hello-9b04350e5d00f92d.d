6b3222adae9157332739f6e9be12eac701828b27
niikun <niikun0209@gmail.com>
2025-03-31 11:28:05 +0900

    first commit

---
+++ mod01/hello/target/debug/deps/hello-9b04350e5d00f92d.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kawata/OneDrive/ドキュメント/fl/rust_fund/mod01/hello/target/debug/deps/hello-9b04350e5d00f92d: src/main.rs
+
+/mnt/c/Users/kawata/OneDrive/ドキュメント/fl/rust_fund/mod01/hello/target/debug/deps/hello-9b04350e5d00f92d.d: src/main.rs
+
+src/main.rs:
---
+++ mod01/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kawata/OneDrive/ドキュメント/fl/rust_fund/mod01/hello/target/debug/hello: /mnt/c/Users/kawata/OneDrive/ドキュメント/fl/rust_fund/mod01/hello/src/main.rs
